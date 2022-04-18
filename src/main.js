aa5fa3cd8294cec6569a098191dd8a223e1bfb81
Anže Oblak <anze.oblak@rettro.si>
2022-04-18 20:45:06 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App';
+import router from './router';
+import BootstrapVue3 from 'bootstrap-vue-3';
+import VueFeather from 'vue-feather';
+import axios from "axios";
+import VueAxios from "vue-axios";
+import moment from "moment";
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+import './assets/styles/style.scss';
+
+const app = createApp(App)
+
+app.use(router);
+app.use(BootstrapVue3);
+app.use(VueAxios, axios);
+
+app.component(VueFeather.name, VueFeather);
+
+app.config.globalProperties.api = {
+    baseUrl: 'http://localhost:5000/api/'
+}
+app.config.globalProperties.$moment = moment();
+
+app.mount('#app')
+
+export default app;
